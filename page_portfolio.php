f4664bcdd74cce8e71546a0f5a9900fe9714099a
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-01 19:29:02 -0400

    Modify CSS title and create portfolio page

---
+++ page_portfolio.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Portfolio Page
+*/
+?>
+
+    <?php get_header(); ?>
+
+        <section class="row">
+          <div class="twelve columns">
+              
+
+          </div>
+
+    <?php get_footer(); ?>
