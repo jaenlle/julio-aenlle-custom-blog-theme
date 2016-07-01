de86b6e224b95d63a40f615538a86ed36f616f42
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-01 02:48:57 -0400

    Add single page content

--- single.php
+++ single.php
@@ -1,24 +1,25 @@
 <?php get_header(); ?>
 
     <section class="row">
-      <div class="twelve columns">
+        <div class="nine columns">
+            <!-- BEGIN PAGE PHP -->
+              <?php
+                if ( have_posts() ) {
+                  while ( have_posts() ){
+                      the_post(); // the_post defines our Data CONTEXT
 
-          <!-- BEGIN PAGE PHP -->
-            <?php
-              if ( have_posts() ) {
-                while ( have_posts() ){
-                    the_post(); // the_post defines our Data CONTEXT
+                      if ( has_post_thumbnail() ){ ?>
+                          <h1 class="idv"><?php the_title(); ?></h2>
+                          <div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
+                      <?php } ?>
 
-                    if ( has_post_thumbnail() ){ ?>
-                        <div class="post-thumbnail"><?php the_post_thumbnail('large'); ?></div>
-                    <?php } ?>
-                        <h2><?php the_title(); ?></h2>
-                    <?php the_content();
-                }//END WHILE
-              } //END IF ?>
-          <!-- END PAGE PHP -->
+                      <?php the_content();
+                  }//END WHILE
+                } //END IF ?>
+            <!-- END PAGE PHP -->
+        </div>
 
-      </div>
+        
     </section>
 
 <?php get_footer(); ?>
