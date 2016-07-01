65f67503057bcff896705dd719c6fafc84732bf5
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-01 02:49:47 -0400

    Add sidebar to single page

--- single.php
+++ single.php
@@ -19,6 +19,18 @@
             <!-- END PAGE PHP -->
         </div>
 
+        <!-- ADDING SIDEBAR -->
+        <div class="three columns">
+            <div id="social">
+                <ul>
+                    <li><a href="#"><img src="http://julioaenlle.net/jaenllewordpress/wp-content/uploads/2016/07/github.png" alt="" /></a></li>
+                    <li><a href="#"><img src="http://julioaenlle.net/jaenllewordpress/wp-content/uploads/2016/07/codepen.png" alt="" /></a></li>
+                    <li><a href="#"><img src="http://julioaenlle.net/jaenllewordpress/wp-content/uploads/2016/07/linkedin.png" alt="" /></a></li>
+                    <li><a href="#"><img src="http://julioaenlle.net/jaenllewordpress/wp-content/uploads/2016/07/twitter.png" alt="" /></a></li>
+                </ul>
+            </div>
+            <?php get_sidebar(); ?>
+        </div>
         
     </section>
 
