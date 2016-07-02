52827b734a1efb84b97488fccc71fd2171958a8a
Julio Aenlle <gmg8r251@ufl.edu>
2016-07-01 20:12:06 -0400

    Modify portfolio page CSS

--- page_portfolio.php
+++ page_portfolio.php
@@ -7,9 +7,21 @@ Template Name: Portfolio Page
     <?php get_header(); ?>
 
         <section class="row">
-          <div class="twelve columns">
-              
+            <div class="twelve columns">
+                <div id="portfoliopage">
+                  <!-- BEGIN LOOP -->
+                    <?php if ( have_posts() ) {
+                        while ( have_posts() ){
+                            the_post(); // the_post defines our Data CONTEXT ?>
 
-          </div>
+                            <h2><?php the_title(); ?></h2>
+                            <?php the_content();
+
+                        } //end while statment
+                    } //end if statement ?>
+                  <!-- END LOOP -->
+                </div>
+            </div>
+        </section>
 
     <?php get_footer(); ?>
